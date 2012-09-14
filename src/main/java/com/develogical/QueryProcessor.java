4cf7a7af5fb63a391cc987367e955eab32a4d9a1
Mark Robert Coleman <m.coleman@uglyduckling.nl>
2012-09-14 14:09:35 +0200

    Changed search query to SPA2013

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -3,7 +3,7 @@ package com.develogical;
 public class QueryProcessor {
 
     public String process(String query) {
-        if (query.contains("SPA2012")) {
+        if (query.contains("SPA2013")) {
             return "SPA is a conference";
         }
         return "";
